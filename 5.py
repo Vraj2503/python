68361b097fb994a8d0270a0f2ec300db85cade10
Vraj2503 <115792188+Vraj2503@users.noreply.github.com>
2023-02-01 12:31:02 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,12 @@
+a=float(input("Enter any number "))
+b=float(input("Enter another number "))
+
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+
+print(a,"+",b,"=",c)
+print(a,"-",b,"=",d)
+print(a,"*",b,"=",e)
+print(a,"/",b,"=",f)
